69d725835a7f329788122437e5fc87ed1cf54c59
Andres <andresrevelo.sk8@gmail.com>
2019-03-15 12:05:07 -0500

    carga disco c

---
+++ scripts_ejemplos/script2.php
@@ -0,0 +1,15 @@
+<?php
+
+  $num1=10;
+  $num2=5;
+  //$suma=$num1 + $num2;
+  $resta=$num1 - $num2;
+  $mul=$num1 * $num2;
+  $div=$num1 / $num2;
+  echo "<center>";
+  echo "la suma es: ".($num1+$num2);
+  echo "<br>la resta es: ".$resta;
+  echo "<br>la multiplicacion es: ".$mul;
+  echo "<br>la division es: ".$div;
+  echo "</center>";
+?>
