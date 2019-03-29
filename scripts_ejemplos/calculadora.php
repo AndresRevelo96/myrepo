762d25e463c4f82d5e32379599b2534b1f539003
andres <andresrevelo.sk8@gmail.com>
2019-03-28 21:11:45 -0500

    edicion

--- scripts_ejemplos/calculadora.php
+++ scripts_ejemplos/calculadora.php
@@ -3,25 +3,32 @@
 $n1= $_POST ['num1'];
 $n2= $_POST ['num2'];
 $op= $_POST ['operar'];
-//echo "Numero 1: ".$n1;
-//echo "<br>Numero 2: ".$n2;
-//echo "<br>Operacion: ".$op;
+echo "Numero 1: ".$n1;
+echo "<br>Numero 2: ".$n2;
+echo "<br>Operacion: ".$op;
+
 switch ($op) {
   case '1':
-    echo "La suma es: ".($n1+$n2);
+      echo "<br>La suma es: ".($n1+$n2);
     break;
     case '2':
-      echo "La Resta es: ".($n1-$n2);
+      echo "<br>La Resta es: ".($n1-$n2);
       break;
     case '3':
-        echo "La Multiplicacion es: ".($n1*$n2);
+      echo "<br>La Multiplicacion es: ".($n1*$n2);
         break;
     case '4':
-          echo "La Division es: ".($n1/$n2);
+      echo "<br>La Division es: ".($n1/$n2);
           break;
     case '5':
-            echo "La Resta es: ".($n1-$n2);
-            break;
+
+          break;
+    case '6':
+
+          break;
+    case '7':
+
+          break;
     default:
   echo "Opcion incorrecta";
     break;
